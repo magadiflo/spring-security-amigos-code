f19013391b976f72ff016198fe76d4952a046d0c
Martín <magadiflo@gmail.com>
2022-08-02 12:33:27 -0500

    In memory user details manager

--- src/main/java/com/magadiflo/app/security/ApplicationSecurityConfig.java
+++ src/main/java/com/magadiflo/app/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.magadiflo.app.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +21,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic(); //Autenticación básica (Basic Auth), si ingresamos por un navegador mostrará un alert donde se debe especificar username and password
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails magadifloUser = User.builder()
+                .username("magadiflo")
+                .password("12345") //Si queremos trabajar sin encriptar la contraseña debemos anteponer el {noop}, ejemplo: "{noop}12345"
+                .roles("STUDENT") //Internamente será: ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(magadifloUser);
     }
 }
